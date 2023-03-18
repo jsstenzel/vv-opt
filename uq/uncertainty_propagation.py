3baf4fb21e183c97b8354067184c4e858e1afaab
June Stenzel <jstenzel@mit.edu>
2023-03-18 14:19:27 -0400

    Added uncertainty_propagation, simple script for analyzing and plotting result of UP problem

---
+++ uq/uncertainty_propagation.py
@@ -0,0 +1,52 @@
+#TODO: collapse this down to just consider 1 spectrograph
+#TODO: modify this to use the same input as sensitivity_model.py
+
+#import argparse
+import os
+import sys
+import shutil
+import csv
+import fileinput
+sys.path.insert(0, "..")
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+import statistics
+
+#datafile is a csv file where each line has format [x0, ..., xi, ..., xn, Y]
+def uncertainty_prop(datafile, doPlot=False, doPrint=False):
+	y = []
+	with open(datafile) as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',')
+		for row in csvreader:
+			y.append(float(row[-1]))
+
+    return uncertainty_prop(y, doPlot, doPrint)
+
+#y_j: list of all model evaluation results, assumed to be single number
+def uncertainty_prop(y_j, doPlot=False, doPrint=False):
+    mean = statistics.mean(y_j)
+    stddev = statistics.stdev(y_j, mean) #note! This is sample stddev, not population stddev. Different n-factor in front
+    
+    if doPrint:
+        print("***Uncertainty propagation results")
+        print("Sample mean:",mean,"Sample standard deviation:",stddev)
+    
+    if doPlot:
+        n, bins, patches = plt.hist(x=y_j, bins='auto', color='#21aad3', alpha=1.0, rwidth=0.85)
+            plt.grid(axis='y', alpha=0.75)
+            plt.ylim(ymax=np.ceil(n.max() / 10) * 10 + n.max()*0.05)
+            #plt.xticks(rotation=90)
+            plt.xlabel("Y")
+            plt.ylabel("Frequency (N=" + str(len(y_j)) + ")")
+            
+            #add vert line
+            plt.axvline(mean, 0, color='b')
+            
+            plt.show()
+    
+    return mean, stddev
+    
+if __name__ == '__main__':  
+    print("uncertainty_propagation.py: Use me to analyze and plot the results of model samples!")
