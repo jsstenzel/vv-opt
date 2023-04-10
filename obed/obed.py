cab5d861c038c2ac3951f44ec06e41234091c24b
June Stenzel <jstenzel@mit.edu>
2023-04-09 23:09:30 -0400

    Implemented U_brute_varH and the initial toy problem

--- obed/obed.py
+++ obed/obed.py
@@ -1,22 +1,82 @@
 #This details an obed script that can be called regardless of the problem
 #so it takes as input some function defintions
 
+#MCMC References:
+#https://twiecki.io/blog/2015/11/10/mcmc-sampling/
+#https://towardsdatascience.com/bayesian-inference-and-markov-chain-monte-carlo-sampling-in-python-bada1beabca7
+
+import numpy as np
+import scipy.stats
 
 """
 This function is part of the OBED problem, solving the utility for a given d
 This may be an inefficient brute-force approach
 This assumes a utility u(d,y,theta) = 1/Var[H(theta|y,d)], minimizing the variance of the HLVA evaluated over the posterior
 A higher function must optimize over the outputs of this model to find d*, the d that maximized U_d
-d      - the design variable vector to calculate the utility for
-exp_fn - function pointer to experiment model, eta(theta, d)
-H_fn   - function pointer to high-level verification activity model
-G_fn   - function pointer to cost model
+d       - the design variable vector to calculate the utility for
+exp_fn  - function pointer to experiment model, eta(theta, d)
+H_fn    - function pointer to high-level verification activity model
+G_fn    - function pointer to cost model
+p_theta_rvs - function pointer to generate samples of the prior distribution p(theta)
+p_theta_pdf - function pointer to the pdf of the prior distribution p(theta)
 """
-def U_brute_varH(d, exp_fn, H_fn, G_fn):
-    N1 = 1000 #number of outer loops, ranging over values of y
+def U_brute_varH(d, exp_fn, H_fn, G_fn, p_theta_rvs, p_theta_pdf, N=1000, burnin=0, lag=1):
+    N1 = N #number of outer loops, ranging over values of y
     
     #Generate a list of y's sampled from likelihood fn, p(y|theta,d)
     #I think you can just do this by running eta(theta,d) over and over - the random epsilon will create the likelihood fn distribution
     #And of course, to get the theta for those executions, you must correspondingly sample from the prior, p(theta)
+    #so you get a N1-long list of values [y, theta]
+    thetas = p_theta_rvs(N1)
+    Y1_list = [exp_fn(theta, d) for theta in thetas]
+    
+    #this outer loop amounts to calculating U over the set of all likely possible data y
+    #If you have actual data, i think you can skip or simplify this loop to just do mcmc at that known data?
     
     for y in Y1_list:
+        #Ok so for each value of y, i need to use MCMC to sample from the posterior
+        #I think each of those values of y is my data? In each case, the best i can do for data is one
+        #evaluation of the likelihood p(y|theta,d) at a value of y. Looping over the p(y|theta|d)p(theta) values of y
+        #makes it valid i think. We'll see
+        
+        N2 = N*lag + burnin #number of samples of the posterior i want, times lag plus burn-in
+        #I will probably have to take into account things like burn-in/convergence and lag? idk
+        
+        theta_current = p_theta_rvs(1)[0]
+        mcmc_trace = []
+        for i in range(N2): #MCMC loop
+            #Propose a new value of theta from the prior
+            theta_proposal = p_theta_rvs(1)[0]
+            #Compute likelihood of the "data" for both thetas - this isnt actually easy
+            #   I dont have a function of theta that returns p(y|theta,d)
+            #   Best I can do is run eta(theta,d) a bunch of times, fit a distribution fn to that, and calculate likelihoods
+            #   "probability density estimation"
+            N3 = N #number of eta calculations I gotta make for good estimates here
+            ylist_current = []
+            ylist_proposal = []
+            for j in range(N3):
+                ylist_current.append(exp_fn(theta_current, d))
+                ylist_proposal.append(exp_fn(theta_proposal, d)) #so now we have simulated pdfs of y
+            likelihood_current = scipy.stats.gaussian_kde(ylist_current).evaluate(y) #using kde to estimate pdf
+            likelihood_proposal = scipy.stats.gaussian_kde(ylist_proposal).evaluate(y) #calc probability at the data y
+            #Compute acceptance ratio
+            prior_current = p_theta_pdf(theta_current)
+            prior_proposal = p_theta_pdf(theta_proposal)
+            p_current = likelihood_current * prior_current
+            p_proposal = likelihood_proposal * prior_proposal
+            R = p_proposal / p_current
+            #Accept our new value of theta according to the acceptance probability R:
+            if np.random.random_sample() < R:
+                theta_current = theta_proposal
+            #Include theta_current in my trace according to rules
+            if i > burnin and i%lag == 0:
+                mcmc_trace.append(theta_current)
+            
+        #Now, use my posterior samples to calculate H(theta|y,d) samples
+        H_theta_posterior = [H_fn(theta) for theta in mcmc_trace]
+        
+        #Now find the variance of that. Return the inverse minus the cost as the utility
+        var_H = np.var(H_theta_posterior, ddof=1) #its a sample variance
+        U = (1/var_H) - G_fn(d)
+        return U, H_theta_posterior
+        
---
+++ problems/simplest_toy/toy_vv_opt.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import scipy.stats
+import math
+import matplotlib.pyplot as plt
+
+sys.path.append('../..')
+from obed.obed import U_brute_varH
+from uq.uncertainty_propagation import uncertainty_prop_plot
+
+def eta(_theta, _d):
+    random = scipy.stats.norm.rvs(loc=0, scale=_d, size=1)[0]
+    return _theta + random
+    
+def H(_theta):
+    return (_theta+1)**2
+    
+def Gamma(_d):
+    return 10/(_d+.01)
+    
+#we'll assume a uniform prior on theta, from 0 to 1
+def Ptheta_rvs(s=1):
+    #sample from the prior probability of theta
+    return scipy.stats.norm.rvs(size=s)
+    
+#def Ptheta_pdf(_theta):
+#    #return the prior probability of theta
+#    if _theta<0 or _theta>1:
+#        return 0
+#    else:
+#        return 1
+
+def Ptheta_pdf(_theta):
+    return scipy.stats.norm.pdf(_theta, loc=0.5, scale=0.5)
+
+#plot the trace of H(posterior)
+d = 0.5
+u, H_trace = U_brute_varH(d, eta, H, Gamma, Ptheta_rvs, Ptheta_pdf, N=1000, burnin=0, lag=1)
+print(u)
+plt.plot(H_trace)
+plt.show()
+uncertainty_prop_plot(H_trace)
+
+
+#find optimal d
+u_list = []
+d_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+for d in d_list:
+    print("d =",d,'...',flush=True)
+    u, _ = U_brute_varH(d, eta, H, Gamma, Ptheta_rvs, Ptheta_pdf, N=300, burnin=0, lag=1)
+    u_list.append(u)
+    
+plt.plot(d_list, u_list)
+plt.show()
