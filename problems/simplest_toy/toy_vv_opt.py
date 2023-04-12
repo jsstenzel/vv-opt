cc6bf0bad2f9bb952e4c29cacfd3506dd33d7d59
June Stenzel <jstenzel@mit.edu>
2023-04-12 16:19:52 -0400

    Reorganized toy problem vv_opt a little, added some analysis for plotting trace of kde solver

--- problems/simplest_toy/toy_vv_opt.py
+++ problems/simplest_toy/toy_vv_opt.py
@@ -3,6 +3,8 @@ import os
 import scipy.stats
 import math
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 sys.path.append('../..')
 from obed.obed import U_brute_varH, U_reloop_varH
@@ -36,7 +38,9 @@ def Ptheta_pdf(_theta):
 def empty(whatever):
     return 0
 
-if False:
+#############################################
+
+def __brute_u_example():
     #plot the trace of H(posterior)
     d = 0.5
     u, H_trace = U_brute_varH(d, eta, H, Gamma, Ptheta_rvs, Ptheta_pdf, N=1000, burnin=0, lag=1)
@@ -45,7 +49,7 @@ if False:
     plt.show()
     uncertainty_prop_plot(H_trace)
 
-if False:
+def __brute_u_find_d():
     #find optimal d
     u_list = []
     d_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
@@ -57,7 +61,7 @@ if False:
     plt.plot(d_list, u_list)
     plt.show()
     
-if False:
+def __brute_u_plot_pareto():
     #find optimal d, plotting out u and Gamma
     u_list = []
     d_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
@@ -78,9 +82,60 @@ if False:
     plt.ylabel("u")
     plt.show()
 
-if True:
+def __reloop_u_example():
     #plot the trace of H(posterior)
     d = 0.5
     u, ulist = U_reloop_varH(d, eta, H, empty, Ptheta_rvs, Ptheta_pdf, n1=1000, n2=500, burnin=0, lag=1) 
     print(u)
     uncertainty_prop_plot(ulist)
+    
+def __reloop_u_plot_pareto():
+    #find optimal d, plotting out u and Gamma
+    u_list = []
+    d_list = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
+    G_list = []
+    for d in d_list:
+        print("d =",d,'...',flush=True)
+        u, _ = U_reloop_varH(d, eta, H, empty, Ptheta_rvs, Ptheta_pdf, n1=1000, n2=500, burnin=0, lag=1)
+        u_list.append(u)
+        G_list.append(Gamma(d))
+        
+    plt.plot(d_list, u_list, 'g')
+    plt.plot(d_list, G_list, 'r')
+    plt.xlabel("d")
+    plt.ylabel("u, Γ")
+    plt.show()
+    plt.plot(G_list,u_list)
+    plt.xlabel("Γ")
+    plt.ylabel("u")
+    plt.show()
+    
+    
+#To try to answer how large n1 needs to be, im going to try plotting trace of gaussian kde
+def __trace_scipy_kde():
+    nmax = 10000
+    d = 0.5
+    thetas = Ptheta_rvs(100)
+    ys = [eta(theta, d) for theta in thetas]
+    
+    test_thetas = Ptheta_rvs(100)
+    test_y_theta = [[eta(theta, d),theta] for theta in test_thetas]
+    trace = []
+    for n in range(nmax):
+        thetas = np.append(thetas, Ptheta_rvs(1)[0])
+        ys = np.append(ys, eta(thetas[-1], d))
+        
+        y_theta_values = np.vstack([ys, thetas])
+        likelihood_kernel = scipy.stats.gaussian_kde(y_theta_values)
+        
+        #grab metrics from this kde
+        #i want to trace some kind of change from one step to the next
+        test_calcs = [likelihood_kernel.pdf(pair)[0] for pair in test_y_theta]
+        
+        trace_metric = sum(test_calcs)
+        trace.append(trace_metric)
+    #print(trace, flush=True)
+    plt.plot(trace)
+    plt.show()
+        
+__trace_scipy_kde()
