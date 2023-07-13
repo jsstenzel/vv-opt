c82cb499c932c72697d26a05923d619bd24f5125
June Stenzel <jstenzel@mit.edu>
2023-07-12 22:03:43 -0700

    Added initial (requirement-based) values for the fp priors

--- problems/fp_verification/fp_vv_opt.py
+++ problems/fp_verification/fp_vv_opt.py
@@ -15,16 +15,24 @@ from problems.fp_verification.fp_experiment_models import *
 
 
 #eta = fp_likelihood_fn
-	
 #H = fp_hlva
-	
 #Gamma = fp_cost			   
 
-fp_theta_defs = [ 
-					("gain", ["gaussian", [0,1]]),
-					("rn",   ["gaussian", [0,1]]),
-					("dc",   ["gaussian", [0,1]]),
+#these priors are based on requirements that were met, see Camera Qual Report
+theta_req_defs = [ 
+					("gain", ["gaussian", [1.0,0.2]]), #mean, stddev
+					("rn",   ["gaussian", [2.5,0.25]]),
+					("dc",   ["gaussian", [0.001,.002]]),
 				]
+#need to update with range somehow? These can't be negative
+
+#for post-fabrication pre-testing priors, I should update the above priors
+#with the Qual data from SN20001 and SN20003
+#theta_qual_defs = [ 
+#					("gain", ["gaussian", [1.0,0.2]]), #mean, stddev
+#					("rn",   ["gaussian", [2.5,0.25]]),
+#					("dc",   ["gaussian", [0.001,.002]]),
+#				]
 
 fp_y_defs = ["y_gain", "y_rn", "y_dc"]
 
@@ -43,15 +51,15 @@ fp_x_defs = [
 				#general
 				("nx", 2048),
 				("ny", 2048),
-				("sigma_dc", .5), #e-/s
-				("sigma_stray", .1) ,
+				("sigma_dc", .5), #e-/s #WAG for now
+				("sigma_stray", .1), #WAG for now
 				#gain
 				("P_signal", 0.95), #Prob. of correctly identifying signal as event #WAG for now
 				("P_noise", 0.01), #Prob. of incorrectly identifying noise/interference as event #WAG for now
 				("T_ccd", _temp), #K
 				("E0", e0), #22.1 keV Cd-109 emission line
 				("sigma_E", math.sqrt((_m0 * _c^2) / (_k*_temp*_e0^2))), #1/eV^2
-				("w", 3.66 + 0.000615*(300-_temp)), #eV/e- #this has uncertainty. i probably need to treat this as a nuisance parameter later?
+				("w", 3.66 + 0.000615*(300-_temp)), #eV/e- #this has uncertainty. nuisance parameter?
 				("rate_cd109", 1.7387e-8), #1/s #based on 461.4 days Cd-109 half life
 				("grade_size", 3), #3x3 event grade sizes
 				#rn
@@ -65,6 +73,6 @@ fp_x_defs = [
 
 
 #_dim_d, _dim_theta, _dim_y, _dim_x, _eta, _H, _G, _x_default, _priors)
-fp = ProblemDefinition(fp_likelihood_fn, fp_hlva, fp_cost, fp_theta_defs, fp_y_defs, fp_d_defs, fp_x_defs)
+fp = ProblemDefinition(fp_likelihood_fn, fp_hlva, fp_cost, theta_req_defs, fp_y_defs, fp_d_defs, fp_x_defs)
 
 print(fp)
