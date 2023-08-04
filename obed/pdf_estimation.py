2824e684ecac981d4e78f2701f8b05de0768dddd
June Stenzel <jstenzel@mit.edu>
2023-08-03 23:06:26 -0700

    Revamped and expanded kde_plot to handle multiple dimensions, and plot sequentially or put it all into a subplot. This works great for a kde of just the y's, but a kde of the joint distribution or the flat-theta 6d likelihood distribution, I still need to figure out how best to handle.

--- obed/pdf_estimation.py
+++ obed/pdf_estimation.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-		
 
 def multivar_likelihood_kernel(d, exp_fn, prior_rvs, n1):
 	thetas = prior_rvs(n1)
@@ -31,26 +30,48 @@ def general_likelihood_kernel(*params):
 	likelihood_kernel = scipy.stats.gaussian_kde(y_theta_values.T)			
 	return likelihood_kernel
 
-#likelihood is the kde generated from sample
+#likelihood is expected to be the kde generated from sample
 #this assumes 3 elements in 7 - i can generalize this easily later
-def kde_plot(likelihood, sample):
-	xdata = [y[0] for y in sample]
-	ydata = [y[1] for y in sample]
-	zdata = [y[2] for y in sample]
+def kde_plot(likelihood, sample, plotStyle='separate', center=None):
+	data = np.transpose(sample)
 	
-	#3d plot
-	#fig = plt.figure()
-	#ax = plt.axes(projection='3d')
-	#ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
+	if center == None:
+		#we have to evaluate at a point in the point in the domain of the pdf
+		#so that we can see all the 2d plots at that point
+		center = [np.mean(idata) for idata in data]
 	
-	xs = np.linspace(min(xdata), max(xdata), len(xdata))
-	plt.plot(xs, [likelihood([x,np.mean(ydata),np.mean(zdata)]) for x in xs])
-	plt.show()
+	if plotStyle == '3d':
+		#3d plot
+		xdata = [y[0] for y in sample]
+		ydata = [y[1] for y in sample]
+		zdata = [y[2] for y in sample]
+		fig = plt.figure()
+		ax = plt.axes(projection='3d')
+		ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
 	
-	ys = np.linspace(min(ydata), max(ydata), len(ydata))
-	plt.plot(ys, [likelihood([np.mean(xdata),y,np.mean(zdata)]) for y in ys])
-	plt.show()
-	
-	zs = np.linspace(min(zdata), max(zdata), len(zdata))
-	plt.plot(zs, [likelihood([np.mean(xdata),np.mean(ydata),z]) for z in zs])
-	plt.show()
+	if plotStyle == 'separate':
+		for i,xdata in enumerate(data):
+			xs = np.linspace(min(xdata), max(xdata), len(xdata))
+			#probs = [likelihood(center but put x at center[i])) for x in xs]
+			probs = [likelihood([x if j==i else center[j] for j,_ in enumerate(data)]) for x in xs]
+			#plt.plot(xs, [likelihood([x,np.mean(ydata),np.mean(zdata)]) for x in xs])
+			plt.plot(xs, probs)
+			plt.show()
+
+	if plotStyle == 'together':
+		fig, axs = plt.subplots(nrows=1, ncols=len(data), sharey='all')
+		for i,xdata in enumerate(data):
+			xs = np.linspace(min(xdata), max(xdata), len(xdata))
+			#probs = [likelihood(center but put x at center[i])) for x in xs]
+			probs = [likelihood([x if j==i else center[j] for j,_ in enumerate(data)]) for x in xs]
+			#plt.plot(xs, [likelihood([x,np.mean(ydata),np.mean(zdata)]) for x in xs])
+			axs[i].plot(xs, probs)
+			axs[i].set_xlabel("Y"+str(i))
+		
+		plt.subplots_adjust(left=0.1,
+							bottom=0.1,
+							right=0.9,
+							top=0.9,
+							wspace=0.0,
+							hspace=0.0)
+		plt.show()
