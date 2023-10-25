bbd30bb15f430fca485fafc323a3aa67fb92704f
June Stenzel <jstenzel@mit.edu>
2023-10-25 12:49:26 -0400

    Starting to implement GMM with sklearn

--- inference/lieberman_willcox_2014_sketch.py
+++ inference/lieberman_willcox_2014_sketch.py
@@ -4,8 +4,9 @@ import sys
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
 
-import sys.path.append('../..')
+sys.path.append('..')
 #focal plane
 from problems.fp_verification.fp_problem import *
 
@@ -32,3 +33,24 @@ def theta_prior_sample(num):
 def theta_prior_pdf(theta):
 	return fp.prior_pdf_unnorm(theta)
 	
+#step 1: train 
+#Get a a Gaussian mixture model from the push-forward of the prior through yd and yp
+
+samples = theta_prior_sample(10000)
+yd_sample = [expt_process(theta) for theta in samples]
+yp_sample = [pred_process(theta) for theta in samples]
+
+#actually i think ill use sklearn for this
+data = np.array([np.hstack([yd_sample[i],yp_sample[i]]) for i,_ in enumerate(samples)])
+
+#need to use a "Bayesian information criterion approach [8]"
+#or, a "number of components to balance the maximization of likelihood of data and the Bayesian information criterion [4]"
+#which one??? the latter seems fine
+#ok, generally to do this, we'll search bottom-up, increasing n_comp until we find a minimal BIC
+for i in [1,2,3,4,5]:
+	ncomp = i
+	gmm = GaussianMixture(n_components=ncomp).fit(data)
+	bic = gmm.bic(data)
+	print(bic)
+
+
