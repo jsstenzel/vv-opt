274c12f3d96df4ae535eb489dca2c28accfc1d7b
June Stenzel <jstenzel@mit.edu>
2023-10-29 15:32:24 -0400

    Fixed my minor problems - mostly with the plotting. Now the algorithm matches the linear example in the thesis, and I'm getting something that looks sane for the focal plane problem.

--- inference/lieberman_willcox_2014_sketch.py
+++ inference/lieberman_willcox_2014_sketch.py
@@ -10,37 +10,11 @@ sys.path.append('..')
 #focal plane
 print("importing fp...",flush=True)
 from problems.fp_verification.fp_problem import *
-
-d_historical = [
-				20,   #t_gain
-				30,   #I_gain
-				1,	#n_meas_rn
-				8,	#d_num
-				9600, #d_max
-				2	 #d_pow   #approx
-			   ]
-
-def expt_process(theta):
-	yd = fp.eta(theta, d_historical)
-	return yd
-
-def pred_process(theta):
-	yp = fp.H(theta)
-	return yp
 	
-def theta_prior_sample(num):
-	return fp.prior_rvs(num)
-	
-theta_nominal = [1.1, 2.5, .001]
-y_nominal = fp_likelihood_fn(dict(zip(fp.theta_names, theta_nominal)), dict(zip(fp.d_names, d_historical)), dict(zip(fp.x_names, fp.x_default)), err=False)
-Yd = y_nominal
-print("Simulated data:", Yd, flush=True)
-print("QoI prediction given that data assuming no error:", pred_process(theta_nominal), flush=True)
-	
-def inference_predictive_posterior(expt_process, pred_process, theta_prior_sample, Yd, nsamples=300000):
+def inference_predictive_posterior(expt_process, pred_process, theta_prior_sample, Yd, nsamples=300000, verbose=0):
 	#step 1: train 
 	#Get a a Gaussian mixture model from the push-forward of the prior through yd and yp
-	print("getting data...",flush=True)
+	print("getting data...",flush=True) if verbose else 0
 	samples = theta_prior_sample(nsamples)
 	yp_sample = [pred_process(theta) for theta in samples]
 	yd_sample = [expt_process(theta) for theta in samples]
@@ -55,13 +29,13 @@ def inference_predictive_posterior(expt_process, pred_process, theta_prior_sampl
 	#or, a "number of components to balance the maximization of likelihood of data and the Bayesian information criterion [4]"
 	#which one??? the latter seems fine
 	#ok, generally to do this, we'll search bottom-up, increasing n_comp until we find a minimal BIC
-	print("calculating ideal ncomp...",flush=True)
+	print("calculating ideal ncomp...",flush=True) if verbose else 0
 	curr_gmm = GaussianMixture(n_components=1).fit(data)
 	curr_bic = curr_gmm.bic(data)
-	print(curr_bic, flush=True)
+	print(curr_bic, flush=True) if verbose==2 else 0
 	next_gmm = GaussianMixture(n_components=2).fit(data)
 	next_bic = next_gmm.bic(data)
-	print(next_bic, flush=True)
+	print(next_bic, flush=True) if verbose==2 else 0
 	ncomp = 1
 	while next_bic < curr_bic:
 		curr_bic = next_bic
@@ -69,9 +43,9 @@ def inference_predictive_posterior(expt_process, pred_process, theta_prior_sampl
 		ncomp += 1
 		next_gmm = GaussianMixture(n_components=ncomp+1).fit(data)
 		next_bic = next_gmm.bic(data)
-		print(next_bic, flush=True)
+		print(next_bic, flush=True) if verbose else 0
 
-	print("Number of components that minimizes BIC is",ncomp, flush=True)
+	print("Number of components that minimizes BIC is",ncomp, flush=True) if verbose else 0
 	#ncomp = 7
 	#gmm = GaussianMixture(n_components=ncomp).fit(data)
 	#we now have our gmm that we use below
@@ -79,19 +53,27 @@ def inference_predictive_posterior(expt_process, pred_process, theta_prior_sampl
 
 	#step 2
 	#Now we have our data, and we find the posterior predictive from that
-	print("calculating posterior predictive...",flush=True)
+	print("calculating posterior predictive...",flush=True) if verbose else 0
 	Yd = np.transpose(np.array(Yd))
 
 	#get key parameters from the GMM
 	mu = np.array(gmm.means_)
 	Sig = np.array(gmm.covariances_)
-	alpha = np.array(gmm.weights_)#; print(alpha)
-	ymean_p = np.array([mu_k[:p_dimension] for mu_k in mu])#; print(ymean_p[0])
-	ymean_d = np.array([np.transpose(mu_k[p_dimension:]) for mu_k in mu])#; print(ymean_d[0])
-	Sig_pp = np.array([Sig_k[:p_dimension, :p_dimension] for Sig_k in Sig])#; print(Sig_pp[0])
-	Sig_pd = np.array([Sig_k[:p_dimension, p_dimension:] for Sig_k in Sig])#; print(Sig_pd[0])
-	Sig_dp = np.array([Sig_k[p_dimension:, :p_dimension] for Sig_k in Sig])#; print(Sig_dp[0])
-	Sig_dd = np.array([Sig_k[p_dimension:, p_dimension:] for Sig_k in Sig])#; print(Sig_dd[0])
+	alpha = np.array(gmm.weights_)
+	ymean_p = np.array([mu_k[:p_dimension] for mu_k in mu])
+	ymean_d = np.array([np.transpose(mu_k[p_dimension:]) for mu_k in mu])
+	Sig_pp = np.array([Sig_k[:p_dimension, :p_dimension] for Sig_k in Sig])
+	Sig_pd = np.array([Sig_k[:p_dimension, p_dimension:] for Sig_k in Sig])
+	Sig_dp = np.array([Sig_k[p_dimension:, :p_dimension] for Sig_k in Sig])
+	Sig_dd = np.array([Sig_k[p_dimension:, p_dimension:] for Sig_k in Sig])
+	
+	if verbose==2:
+		print("ymean_p:", ymean_p)
+		print("ymean_d:", ymean_d)
+		print("Sig_pp:\n", Sig_pp)
+		print("Sig_pd:\n", Sig_pd)
+		print("Sig_dp:\n", Sig_dp)
+		print("Sig_dd:\n", Sig_dd, flush=True)
 
 	#parameters for the new GMM:
 	B1 = [alpha[k] * (2*math.pi)**(-ncomp/2.0) * np.linalg.det(Sig_dd[k])**(-0.5) 
@@ -101,14 +83,20 @@ def inference_predictive_posterior(expt_process, pred_process, theta_prior_sampl
 	beta = np.array([B1[k] / B0 for k in range(ncomp)])
 	mu_Yd = np.array([ymean_p[k] + Sig_pd[k] @ np.linalg.inv(Sig_dd[k]) @ (Yd - ymean_d[k]) for k in range(ncomp)])
 	Sig_Yd = np.array([Sig_pp[k] - Sig_pd[k] @ np.linalg.inv(Sig_dd[k]) @ Sig_dp[k] for k in range(ncomp)])
+	print(Sig_pp[0], Sig_pd[0] @ np.linalg.inv(Sig_dd[0]) @ Sig_dp[0])
+	
+	if verbose==2:
+		print("beta:", beta)
+		print("mu_Yd:", mu_Yd)
+		print("Sig_Yd:", Sig_Yd, flush=True)
 	
 	return beta, mu_Yd, Sig_Yd
 	
 def plot_predictive_posterior(beta, mu_Yd, Sig_Yd, lbound, rbound):
 	#p is one-dimensional, give it a plot
-	x = np.linspace(0, 1, 10000)
+	x = np.linspace(lbound, rbound, 10000)
 
-	pdfs = np.array([p * scipy.stats.norm.pdf(x, mu, sd) for mu, sd, p in zip(mu_Yd, Sig_Yd, beta)])
+	pdfs = np.array([p * scipy.stats.norm.pdf(x=x, loc=mu, scale=np.sqrt(sd)) for mu, sd, p in zip(mu_Yd, Sig_Yd, beta)])
 	pdfs = np.array([pdf[0] for pdf in pdfs])
 	density = np.sum(np.array(pdfs), axis=0)
 
@@ -119,7 +107,7 @@ def plot_predictive_posterior(beta, mu_Yd, Sig_Yd, lbound, rbound):
 	plt.ylabel('$f(y_p | y_d)$')
 	plt.show()
 
-#i need to validate this algorithm - check [17], original thesis, linear model 5.5.1:
+###Validate this algorithm - check [17], original thesis, linear model 5.5.1:
 def linear_expt(theta):
 	mu1 = theta[0]
 	mu2 = theta[1]
@@ -133,23 +121,47 @@ def linear_pred(theta):
 def linear_prior(num):
 	return [scipy.stats.norm.rvs(size=2) for _ in range(num)]
 
+
 theta_true = [.25,.25]#linear_expt(linear_prior(1)[0])
 Yd = [1.25,0] #linear_expt(theta_true)
 print(Yd)
-a,b,c = inference_predictive_posterior(linear_expt, linear_pred, linear_prior, Yd, nsamples=50000)
-print(a)
-print(b)
-print(c)
+a,b,c = inference_predictive_posterior(linear_expt, linear_pred, linear_prior, Yd, nsamples=50000, verbose=2)
 
 x = np.linspace(0, 1, 10000)
-y = scipy.stats.norm.pdf(x, .4*Yd[0] - .2*Yd[1], np.sqrt(.0015))
+y = scipy.stats.norm.pdf(x, .4*Yd[0] - .2*Yd[1], np.sqrt(.0005)) #do the math, punk!
 plt.plot(x,y,c='red')
 plot_predictive_posterior(a, b, c, -1, 1)
 
 
+"""
+###Now try the focal plane case:
+d_historical = [
+				20,   #t_gain
+				30,   #I_gain
+				1,	#n_meas_rn
+				8,	#d_num
+				9600, #d_max
+				2	 #d_pow   #approx
+			   ]
 
+def expt_fp(theta):
+	yd = fp.eta(theta, d_historical)
+	return yd
 
+def pred_fp(theta):
+	yp = fp.H(theta)
+	return yp
+	
+def theta_prior_fp(num):
+	return fp.prior_rvs(num)
+	
+theta_nominal = [1.1, 2.5, .001]
+y_nominal = fp_likelihood_fn(dict(zip(fp.theta_names, theta_nominal)), dict(zip(fp.d_names, d_historical)), dict(zip(fp.x_names, fp.x_default)), err=False)
+Yd = y_nominal
+print("Simulated data:", Yd, flush=True)
+print("QoI prediction given that data assuming no error:", pred_fp(theta_nominal), flush=True)
 
-
-
+a,b,c = inference_predictive_posterior(expt_fp, pred_fp, theta_prior_fp, Yd, nsamples=100000, verbose=2)
+plot_predictive_posterior(a, b, c, 0, 7)
+"""
 
