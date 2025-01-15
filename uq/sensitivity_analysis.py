f7d63d72622b95a7ed955ddecef67646d23590c7
June Stenzel <jstenzel@mit.edu>
2025-01-14 22:36:02 -0500

    Radically re-arranged sensitivity_analysis
    - Broke up the big function into smaller module functions. Maintains previous behavior, but now smoother and with less redundancy
    - Added a new method for resampling, basically allowing me to re-use the previous model outputs when I want to re-run SA with larger N
    - Added a unit test in the file for the Ishigami function. Alarmingly, it is not giving the correct indices... I may need to use a whole different library ):

--- uq/sensitivity_analysis.py
+++ uq/sensitivity_analysis.py
@@ -20,7 +20,7 @@ from problems.problem_definition import *
 num_workers = mp.cpu_count() 
 
 
-def sobol_saltelli_file(datafile, var_names, doSijCalc=False, doPlot=False, doPrint=False, doReport=''):
+def sobol_saltelli_file(datafile, var_names, var_dists, var_bounds, doSijCalc=False, doPlot=False, doPrint=False, writeReport=''):
 	y = []
 	x = []
 	with open(datafile) as csvfile:
@@ -30,12 +30,44 @@ def sobol_saltelli_file(datafile, var_names, doSijCalc=False, doPlot=False, doPr
 			y.append(float(row[-1]))
 	#print(x)
 	#print(y)
+	
+	problem = sobol_preprocess(var_names, var_dists, var_bounds)
 
-	sobol_saltelli(x, var_names, y, doSijCalc, doPlot, doPrint, doReport)
+	###calculate indices
+	sobol_analyze_samples(problem, y, doSijCalc, conf, doPrint, writeReport)
+	
+	if doPlot:
+		plot_gsa(var_names, Si=Si, logplot=True)
+	
+	return Si
+
 
 #based on https://salib.readthedocs.io/en/latest/user_guide/basics_with_interface.html
 #Generates samples, generates Y values, runs the SA
-def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, doSijCalc=False, doPlot=False, doPrint=False, doReport=''):
+def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, doSijCalc=False, doPlot=False, doPrint=False, writeSamples='', writeReport=''):
+	#check dimensions
+	#Handle the dists:
+	problem = sobol_preprocess(var_names, var_dists, var_bounds)
+	
+	#Handle the dists:
+	###generate param values
+	#apply the distributions unsupported by SALib
+	###run model
+	#write samples
+	param_values, Y = sobol_generate_samples(function, N, problem, var_dists, var_bounds, doSijCalc=False, doPrint=False, writeSamples=writeSamples)
+	
+	###calculate indices
+	Si = sobol_analyze_samples(problem, Y, doSijCalc, conf, doPrint, writeReport)
+	
+	if doPlot:
+		plot_gsa(var_names, Si=Si, logplot=True)
+	
+	return Si
+
+#Take in the provided names/dists/bounds,
+#check if they're valid
+#and make the subsistutions necessary for unsupported dists
+def sobol_preprocess(var_names, var_dists, var_bounds):
 	#check dimensions
 	if len(var_names) != len(var_dists) or len(var_names) != len(var_bounds):
 		print("ERROR: sobol_saltelli input problem: mismatching number of model evals")
@@ -78,10 +110,15 @@ def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, d
 		'dists': dists
 	}
 	
+	return problem
+
+
+def sobol_generate_samples(function, N, problem, var_dists, var_bounds, doSijCalc=False, doPrint=False, writeSamples=''):	
 	###generate param values
 	if doPrint:
 		print("Generating Sobol sequence sample...", flush=True)
 	param_values = saltelli.sample(problem, N, calc_second_order=doSijCalc)
+	print(N, len(param_values))
 	
 	#apply the distributions unsupported by SALib
 	#under advice by https://stackoverflow.com/questions/45280278/could-salib-support-other-probability-distribution-when-inputing-parameters-in-s
@@ -102,7 +139,7 @@ def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, d
 			param_values[:,j] = scipy.stats.beta.ppf(param_values[:,j], a=a, b=b)
 		if dist == 'nonrandom':
 			param_values[:,j] = np.repeat(var_bounds[j][0], len(param_values[:,j]))
-	
+
 	###run model
 	if doPrint:
 		print("Running the model over the Sobol samples...", flush=True)
@@ -113,7 +150,21 @@ def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, d
 		Y = np.array(Yget)
 	else:
 		Y = np.array([function(x) for x in param_values])
-	
+
+	#write samples
+	if writeSamples != '':
+		#make a csv file where each line has format [x0, ..., xi, ..., xn, Y]
+		writelist = [np.append(val_list, yi) for val_list, yi in zip(param_values,Y)]
+		
+		with open(writeSamples, 'w+', newline='') as csvfile:
+			writer = csv.writer(csvfile)
+			for row in writelist:
+				writer.writerow(row)
+				
+	return param_values, Y
+
+
+def sobol_analyze_samples(problem, Y, doSijCalc, conf, doPrint=False, writeReport=''):
 	###calculate indices
 	if doPrint:
 		print("Analyzing Sobol indices...", flush=True)
@@ -125,59 +176,99 @@ def sobol_saltelli(function, N, var_names, var_dists, var_bounds, conf = 0.95, d
 		#print("ST:\t",Si['ST'],flush=True)
 		#print("ST_conf:\t",Si['ST_conf'],flush=True)
 	
-	if doPlot:
-		plot_gsa(var_names, Si=Si, logplot=True)
-	
-	if doReport != '':
-		with open(doReport+'.csv', 'w+', newline='') as csvfile:
+	if writeReport != '':
+		with open(writeReport, 'w+', newline='') as csvfile:
 			writer = csv.writer(csvfile)
 			for i,_ in enumerate(Si['S1']):
 				row = [Si['S1'][i], Si['S1_conf'][i], Si['ST'][i], Si['ST_conf'][i]]
 				if doSijCalc:
 					row.append([Si['S2'][i], Si['S2_conf'][i]])
 				writer.writerow(row)
-				
+	
 	return Si
 
 
-#This function crucially assumes that the Ys you put in were sampled according to the sobol method
-def sobol_saltelli_presampled(y_list, var_names, conf = 0.95, doSijCalc=False, doPlot=False, doPrint=False, doReport=''):
-	Y = np.array(y_list)
 
-	dim = len(var_names)
-	problem = { #this needs to inherit from problem definition?
-		'names': var_names,
-		'num_vars': dim,
-		'bounds': [[0, 1]]*dim, #fix
-		'dists': ['norm']*dim #fix
-	}
-
-	###calculate indices
-	Si = sobol.analyze(problem, Y, calc_second_order=doSijCalc, conf_level=conf, print_to_console=doPrint, parallel=False, n_processors=None) 
+def sobol_resample(sample_datafile, function, N, problem, var_dists, var_bounds, doSijCalc=False, doPrint=False, doUpdateFile=True):	
+	###Get the currently-existing samples
+	y_read = []
+	x_read = []
+	with open(sample_datafile, 'r') as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=',')
+		for row in csvreader:
+			x_read.append([float(xi) for xi in row[0:-1]])
+			y_read.append(float(row[-1]))
+	#print(x_read)
+	#print(y_read)
+	
+	###Compare N_prev to N
+	length_prev = len(y_read)
+	
+	#From documentation:
+	#If calc_second_order is False, the resulting matrix has N * (D + 2) rows, where D is the number of parameters.
+	#If calc_second_order is True, the resulting matrix has N * (2D + 2) rows
+	d = len(x_read[0])
+	N_prev = int(length_prev/(2*d+2)) if doSijCalc else int(length_prev/(d+2))
+	#expected_length = N_prev*(2*d+2) if doSijCalc else N_prev*(d+2)
+	
+	if not (N_prev > 0 and (N_prev & (N_prev - 1)) == 0):
+		print("Provided sample file",sample_datafile,"is not a power of 2: N_prev =",N_prev)
+		exit()
+	if N <= N_prev:
+		print("N ("+str(N)+") is expected to be a larger power of 2 than N_prev ("+str(N_prev)+").")
+		exit()
+	
+	###generate N param values, even though that involves some redundancy
+	#best i can do if I stick with this library
 	if doPrint:
-		print("Confidence levels on each parameter calculated at ",conf,flush=True)
-		#print("S1:\t",Si['S1'],flush=True)
-		#print("S1_conf:\t",Si['S1_conf'],flush=True)
-		#print("ST:\t",Si['ST'],flush=True)
-		#print("ST_conf:\t",Si['ST_conf'],flush=True)
+		print("Re-generating Sobol sequence sample...", flush=True)
+	param_values = saltelli.sample(problem, N, calc_second_order=doSijCalc)
 	
-	if doPlot:
-		#decide to logplot if the indices span 2 orders of magnitude
-		checklist = [S for S in Si['ST'] if S >1e-12]
-		ST_min = np.min(checklist)
-		ST_max = np.max(checklist)
-		plot_gsa(var_names, Si=Si, logplot=(ST_max/ST_min > 100))
-	
-	if doReport != '':
-		with open(doReport+'.csv', 'w+', newline='') as csvfile:
+	###apply the distributions unsupported by SALib
+	#under advice by https://stackoverflow.com/questions/45280278/could-salib-support-other-probability-distribution-when-inputing-parameters-in-s
+	for j,dist in enumerate(var_dists):
+		if dist == 'gamma_mv':
+			mean = var_bounds[j][0]
+			variance = var_bounds[j][1]
+			alpha = mean**2 / variance
+			beta = mean / variance
+			param_values[:,j] = scipy.stats.gamma.ppf(param_values[:,j], a=alpha, scale=1.0/beta)
+		if dist == 'gamma_ab':
+			alpha = var_bounds[j][0]
+			beta = var_bounds[j][1]
+			param_values[:,j] = scipy.stats.gamma.ppf(param_values[:,j], a=alpha, scale=1.0/beta)
+		if dist == 'beta':
+			a = var_bounds[j][0]
+			b = var_bounds[j][1]
+			param_values[:,j] = scipy.stats.beta.ppf(param_values[:,j], a=a, b=b)
+		if dist == 'nonrandom':
+			param_values[:,j] = np.repeat(var_bounds[j][0], len(param_values[:,j]))
+
+	###run model only for the new points
+	if doPrint:
+		print("Running the model over the new Sobol samples...", flush=True)
+	Yget = []	
+	for ii,xx in enumerate(param_values):
+		if ii < length_prev:
+			Yget.append(y_read[ii])
+			#Grab the already-generated model eval -- this is how we can save time when extending a sequence
+		else:
+			Yget.append(function(xx))
+			if doPrint:
+				print(str(ii+1)+"/"+str(len(param_values)),'\t', flush=True, end='\r')
+	Y = np.array(Yget)
+
+	###write these over the provided samples
+	if doUpdateFile:
+		#make a csv file where each line has format [x0, ..., xi, ..., xn, Y]
+		writelist = [np.append(val_list, yi) for val_list, yi in zip(param_values,Y)]
+		
+		with open(sample_datafile, 'w', newline='') as csvfile:
 			writer = csv.writer(csvfile)
-			for i,_ in enumerate(Si['S1']):
-				row = [Si['S1'][i], Si['S1_conf'][i], Si['ST'][i], Si['ST_conf'][i]]
-				if doSijCalc:
-					row.append([Si['S2'][i], Si['S2_conf'][i]])
+			for row in writelist:
 				writer.writerow(row)
 				
-	return Si
+	return param_values, Y
 
 
 
@@ -219,10 +310,68 @@ def plot_gsa(varnames, Si=None, filename=None, logplot=False):
 	plt.grid(axis = 'y')
 	
 	plt.show()
-	
 
 	
+
+def __ishigami(X):
+	a=7
+	b=0.1
+	x1 = X[0]
+	x2 = X[1]
+	x3 = X[2]
+	return np.sin(x1) + a * np.sin(x2)**2 + b * x3**4 * np.sin(x1)
 	
+#According to https://uqpyproject.readthedocs.io/en/stable/auto_examples/sensitivity/comparison/ishigami.html#sphx-glr-auto-examples-sensitivity-comparison-ishigami-py
+#I should expect the following first-order indices:
+#S1 = 0.3139
+#S2 = 0.4424
+#S3 = 0.0
+#and the following total-order indices:
+#ST1 = 0.55758886
+#ST2 = 0.44241114
+#ST3 = 0.24368366
+
 if __name__ == '__main__':  
+	###########################
+	#Full demonstration
+	###########################
+	
+	#Set up the problem
 	var_names = ['x1','x2','x3']
-	sobol_saltelli_file('testdata.csv', var_names, doSijCalc=True, doPlot=True, doPrint=True, doReport='testreport')
+	dists = ['norm', 'norm', 'norm']
+	bounds = [[-np.pi,np.pi],[-np.pi,np.pi],[-np.pi,np.pi]]
+	
+	dim = len(var_names)
+	problem = { #this needs to inherit from problem definition?
+		'names': var_names,
+		'num_vars': dim,
+		'bounds': bounds,
+		'dists': dists
+	}
+	
+	conf = 0.95
+	sample_file = "test_samples.csv"
+	plist = np.arange(1,16)
+	Si_list = []
+	
+	###Do loop of calculating indices and re-sampling
+	for j,p in enumerate(plist):
+		N = 2**p
+		
+		#Generate/re-generate samples
+		if j==0:
+			###Generate the initial samples	
+			param_values, Y = sobol_generate_samples(__ishigami, 2**1, problem, dists, bounds, doSijCalc=False, doPrint=True, writeSamples=sample_file)
+		else:
+			param_values, Y = sobol_resample(sample_file, __ishigami, N, problem, dists, bounds, doSijCalc=False, doPrint=True, doUpdateFile=True)
+		
+		#calculate indices
+		Si = sobol_analyze_samples(problem, Y, doSijCalc=False, conf=conf, doPrint=True)
+		Si_list.append(Si["S1"])
+		
+	
+	###Plot convergence
+	for i,var_name in enumerate(var_names):
+		plt.plot(plist, [Si[i] for Si in Si_list])
+	plt.legend(var_names)
+	plt.show()
